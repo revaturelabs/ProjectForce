007471462568ed53825795411db9bf034db28ed0
Geoffrey Murray <Geoffrey.Murray.1995@gmail.com>
2019-10-11 09:48:10 -0400

    Moved Object Defs to PFBaseObjects

---
+++ modal/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,16 @@
+declare module "@salesforce/resourceUrl/ChartJS" {
+    var ChartJS: string;
+    export default ChartJS;
+}
+declare module "@salesforce/resourceUrl/JSGanttImproved" {
+    var JSGanttImproved: string;
+    export default JSGanttImproved;
+}
+declare module "@salesforce/resourceUrl/JSGanttImprovedv2" {
+    var JSGanttImprovedv2: string;
+    export default JSGanttImprovedv2;
+}
+declare module "@salesforce/resourceUrl/jQuery" {
+    var jQuery: string;
+    export default jQuery;
+}
