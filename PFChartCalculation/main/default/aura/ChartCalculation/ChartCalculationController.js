ab77803ff7467b06c2d0b1c6cafc33b4925315d6
mruizrevature <matthew.ruiz@revature.com>
2019-11-04 10:50:28 -0500

    removed package folders

--- PFChartCalculation/main/default/aura/ChartCalculation/ChartCalculationController.js
+++
@@ -1,7 +0,0 @@
-({
-	init : function(component, event, helper) {
-		helper.countProjects(component, event);
-		helper.overlappedProjects(component, event);
-		helper.numberOfProjects(component, event);
-	}
-})
--- PFChartComponent/main/default/aura/newD3App/newD3App.app
+++
@@ -1,5 +0,0 @@
-<aura:application extends="force:slds">
-    <!--Just implementing the d3 component-->
-    <c:newD3Comp/>
-    
-</aura:application>
