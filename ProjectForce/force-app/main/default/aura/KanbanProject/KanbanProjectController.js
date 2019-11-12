f236d4a2bb4c68becee0eec28d1317a3fdb0368e
Tinsae <tigial3535@gmail.com>
2019-11-12 15:21:28 -0500

    added page layouts to manifest file

--- ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
+++ ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
@@ -2,7 +2,7 @@
     doInit: function (component, event, helper) {
         var action = component.get("c.getBacklogs");
         var project = component.get("v.project");
-        action.setParams({project: projgect });
+        action.setParams({project: project });
         // Add callback behavior for when response is received
         action.setCallback(this, function (response) {
             var state = response.getState();
