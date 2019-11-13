d5ebe14d4c93acf3d7ccf029521260bf8155716f
mikeHartzell95 <michael.hartzell@revature.net>
2019-11-13 12:52:24 -0500

    made deselection work

--- ProjectForce/force-app/main/default/aura/BatchAndProjectTables/BatchAndProjectTablesController.js
+++ ProjectForce/force-app/main/default/aura/BatchAndProjectTables/BatchAndProjectTablesController.js
@@ -15,7 +15,7 @@
             tableAuraId = "project";
         }
         else if(tableObject=="Project__c"){
-           selectionIds = helper.getBatchIds(event);
+           selectionIds = helper.getBatchIds(component);
            tableAuraId = "batch";
         }
         helper.markTableItemsAsSelected(selectionIds, tableAuraId, component);
@@ -24,31 +24,8 @@
 
 
     handleSaveInlineEditsEvent : function(component, event, helper){
-        var modifiedElements = event.getParam("editedItems");
-        var modifiedTable = event.getParam("editedObject");
-        var tableAuraId;
-        if(modifiedTable==='Training__c'){
-            tableAuraId = 'batch';
-        }
-        else if(modifiedTable==='Project__c'){
-            tableAuraId = "project";
-        }
-        var newTableData = component.find(tableAuraId).get("v.data");
-        var dataToUpdate = [];
-        var keys = Object.keys(newTableData[0]);
-        for(let i=0;i<modifiedElements.length;i++){
-            var rowNumber = modifiedElements[i].id.split('-')[1];
-            dataToUpdate[i] = {};
-            dataToUpdate[i].Id = newTableData[rowNumber].Id;
-            for(let j=0;j<keys.length;j++){
-                var currentKey = keys[j];
-                var currentValue = modifiedElements[i][currentKey];
-                if(currentValue && currentKey!='Id'){
-                    dataToUpdate[i][currentKey] = currentValue;
-                    newTableData[rowNumber][currentKey] = currentValue;
-                }
-            }
-        }
-       helper.saveInlineEdits(component, dataToUpdate, newTableData, tableAuraId);
+        var tableAuraId = helper.getTableAuraId(event);
+        var updatedData = helper.getUpdatedData(component, event, tableAuraId); 
+        helper.saveInlineEdits(component, updatedData[0], updatedData[1], tableAuraId);
     }
 })
