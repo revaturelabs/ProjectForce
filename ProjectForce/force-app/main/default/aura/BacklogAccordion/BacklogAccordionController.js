acb8aa3e8256b82e7110ec4c2d7aa80b0e6556a0
jiphan <james.phan@revature.net>
2019-11-13 12:13:54 -0500

    added hyperlink to record detail page

--- ProjectForce/force-app/main/default/aura/BacklogAccordion/BacklogAccordionController.js
+++ ProjectForce/force-app/main/default/aura/BacklogAccordion/BacklogAccordionController.js
@@ -12,12 +12,18 @@
                 response.getReturnValue().forEach(function(record) {
                     if (record.Stage__c && categories.indexOf(record.Stage__c) === -1) {
                         categories.push(record.Stage__c);
-                    }
+                    } 
+
                     let d = new Date(record.StartDateTime__c);
                     record.StartDateTime__c = d.toLocaleDateString();
+                    console.log(typeof record);
+                    // record.set('stime', d.toLocaleTimeString());
                     d = new Date(record.EndDateTime__c);
                     record.EndDateTime__c = d.toLocaleDateString();
+                    // record.set('etime', d.toLocaleTimeString());
                 });
+                
+                categories.sort(); categories.reverse();
                 component.set('v.categories', categories);
                 component.set('v.records', response.getReturnValue());
             }
