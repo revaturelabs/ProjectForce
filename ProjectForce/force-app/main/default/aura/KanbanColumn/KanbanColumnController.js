c15e9c9e4cb60791b1e30b621d2c1a4cd019c5cd
mariamjose <aleena.jose@revature.net>
2019-11-14 13:53:43 -0500

    Added Start and End Date

--- ProjectForce/force-app/main/default/aura/KanbanColumn/KanbanColumnController.js
+++ ProjectForce/force-app/main/default/aura/KanbanColumn/KanbanColumnController.js
@@ -8,9 +8,9 @@
         var droppedOnColumnEvent = component.getEvent('droppedOnColumn');
         droppedOnColumnEvent.setParams({
             'title': component.get('v.title'),
-            'backlog': JSON.parse(event.dataTransfer.getData('DraggedBacklog'))
+            'backlog': JSON.parse(event.dataTransfer.getData('DraggedBacklog')),
         });
         droppedOnColumnEvent.fire();
-        
+        console.log('title in KanbanColumnController is ',title);
     },
 })
--- ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
+++ ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
@@ -1,6 +1,8 @@
 ({
     doInit: function (component, event, helper) {
         var action = component.get("c.getBacklogs");
+        var project = component.get("v.project");
+        action.setParams({project: project });
         // Add callback behavior for when response is received
         action.setCallback(this, function (response) {
             var state = response.getState();
@@ -12,30 +14,45 @@
                 console.log("Failed with state: " + state);
             }
         });
-        
+
         // Send action off to be executed
         $A.enqueueAction(action);
     },
 
     droppedHandler: function (component, event, helper) {
         var title = event.getParam('title');
-        var backlog = event.getParam('backlog');
+        var backlog = event.getParam('backlog'); //title and backlog after drag
         var listOfBacklogs = component.get('v.backlogs');
 
 
-        var theBacklog = listOfBacklogs.find(function (el) {
-            return el.Story__c == backlog.Story__c;
+        var theBacklog = listOfBacklogs.find(function (el) {// theBacklog is the one that we found
+            return el.Story__c == backlog.Story__c; // using story instead of id
         });
-        if (theBacklog) {
+        if (theBacklog) { //truthy
             theBacklog.Stage__c = title;
-            component.set('v.backlogs', listOfBacklogs);
+            component.set('v.backlogs', listOfBacklogs); //setting params
+
+            //setting the date
+           var today = new Date();
+           if(title == 'Doing'){
+                theBacklog.StartDateTime__c=today;
+                theBacklog.EndDateTime__c=null;
+            }else if(title == 'Done'){
+                if(theBacklog.StartDateTime__c==null){
+                    theBacklog.StartDateTime__c=today;
+                }
+                theBacklog.EndDateTime__c=today;
+            }else if(title == 'To Do'){
+                theBacklog.StartDateTime__c=null;
+                theBacklog.EndDateTime__c=null;
+            }
 
             // update database
             var action = component.get("c.saveBacklog");
-            action.setParams({
-                "backlog": theBacklog
+            action.setParams({ // setting params for apex controller
+                "backlog": theBacklog //setting in apex controller class
             });
-            action.setCallback(this, function(response){
+            action.setCallback(this, function(response){ // calling saveBacklog function
                 var state = response.getState();
                 if (state === "SUCCESS") {
                     console.log('updated');
@@ -46,4 +63,4 @@
             console.log('Could not find this', backlog);
         }
     }
-})
+}) 
