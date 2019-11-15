763756801398e39caeda22b29c0f8d06f3968af0
JayBeeA77 <jbruceattah@yahoo.com>
2019-11-15 10:42:45 -0500

    Working on bug

--- ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
+++ ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
@@ -17,6 +17,7 @@
         
         // Send action off to be executed
         $A.enqueueAction(action);
+        component.set('v.EndDate','10/10/2019');
     },
 
     droppedHandler: function (component, event, helper) {
@@ -50,26 +51,22 @@
     },
     openModel: function (component, event, helper) {
             // for Display Model,set the "isOpen" attribute to "true"
-            component.set("v.isOpen", true);
+            helper.openModel(component);
         },
 
-        closeModel: function (component, event, helper) {
-            // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
-            component.set("v.isOpen", false);
-        },
+    closeModel: function (component, event, helper) {
+        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
+        helper.closeModel(component);
 
-        saveCard: function (component, event, helper) {
-            // Display alert message on the click on the "Like and Close" button from Model Footer 
-            // and set set the "isOpen" attribute to "False for close the model Box.
-            var Story = component.get('v.Story');
-            alert(Story);
+    },
 
-
-
-
-            
-            component.set("v.isOpen", false);
-        },
+    saveCard: function (component, event, helper) {
+        // Display alert message on the click on the "Like and Close" button from Model Footer 
+        // and set set the "isOpen" attribute to "False for close the model Box.
+        var Story = component.get('v.Story');
+        console.log(Story);
+        helper.closeModel(component);
+    },
 
 
     
--- ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectHelper.js
+++ ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectHelper.js
@@ -1,5 +1,13 @@
 ({
-    helperMethod : function() {
+    openModel: function (component) {
+            // for Display Model,set the "isOpen" attribute to "true"
+            
+            component.set("v.isOpen", true);
+        },
 
-    }
+    closeModel: function (component) {
+        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
+        component.set("v.isOpen", false);
+
+    },
 })
