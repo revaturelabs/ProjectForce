f3964ba32cd34b260b064e38fff26afa1e3af5ea
JayBeeA77 <jbruceattah@yahoo.com>
2019-11-15 13:28:57 -0500

    front end first trial

--- ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
+++ ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectController.js
@@ -63,9 +63,42 @@
     saveCard: function (component, event, helper) {
         // Display alert message on the click on the "Like and Close" button from Model Footer 
         // and set set the "isOpen" attribute to "False for close the model Box.
-        var Story = component.get('v.Story');
-        console.log(Story);
-        helper.closeModel(component);
+        var stage = component.get('v.stage');
+        var story = component.get('v.story');
+        var l1 = component.get('v.label1');
+        var l2 = component.get('v.label2');
+        var l3 = component.get('v.label3');
+        var c1 = component.get('v.color1');
+        var c2 = component.get('v.color2');
+        var c3 = component.get('v.color3');
+
+
+        if (story === undefined || story === "" ){
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "title": "Save Failed",
+                "message": "Please make sure you have filled story."
+            });
+            toastEvent.fire();
+        }
+
+        else {
+
+            console.log('Stage: ' + stage);
+            console.log('Story: ' + story);
+            console.log('Label 1: ' + l1);
+            console.log('Label 2: ' + l2);
+            console.log('Label 3: ' + l3);
+            console.log('Color 1: ' + c1);
+            console.log('Color 2: ' + c2);
+            console.log('Color 3: ' + c3);
+            helper.closeModel(component);
+
+        }
+        
+
+
+        
     },
 
 
--- ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectHelper.js
+++ ProjectForce/force-app/main/default/aura/KanbanProject/KanbanProjectHelper.js
@@ -1,13 +1,21 @@
 ({
     openModel: function (component) {
             // for Display Model,set the "isOpen" attribute to "true"
-            
             component.set("v.isOpen", true);
         },
 
     closeModel: function (component) {
-        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
-        component.set("v.isOpen", false);
+        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"
 
+        // component.set("v.Stage", "");
+        component.set("v.story", "");
+        component.set("v.label1", "");
+        component.set("v.label2", "");
+        component.set("v.label3", "");
+        component.set("v.color1", "");
+        component.set("v.color2", "");
+        component.set("v.color3", "");
+        
+        component.set("v.isOpen", false);
     },
 })
