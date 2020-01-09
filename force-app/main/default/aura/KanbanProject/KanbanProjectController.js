0b1d8483d62269318709f35d0f63d01765b42cef
Francesco Ward <francesco@Francesco.local>
2020-01-09 10:40:29 -0500

    add/remove column functionality

--- force-app/main/default/aura/KanbanProject/KanbanProjectController.js
+++ force-app/main/default/aura/KanbanProject/KanbanProjectController.js
@@ -142,45 +142,36 @@
         helper.closeModel(component);
 
     },
+    //added for delte column
+    openModelDeleteTheColumn: function (component, event, helper) {
+        // for Display Model,set the "isOpen" attribute to "true"
+
+        helper.openModelDelCol(component);
+
+        var lab2 = component.find("label2");
+        console.log(lab2);
+
+        $A.util.addClass(lab2, 'toggle');
+
+
+        // component.get("label3").className = "slds-hidden";
+        // console.log(component.find("label2"));
+
+    },
+
+    closeModelDeleteTheColumn: function (component, event, helper) {
+        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
+        helper.closeModelDelCol(component);
+
+    },
 
     saveColumn: function (component, event, helper) {
-        /*
-        var story = component.get('v.story');
-
-        if (story === undefined || story === "" ){
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                "title": "Save Failed",
-                "message": "Please make sure you have filled story."
-            });
-            toastEvent.fire();
-        }
-
-        else {
-*/
+        
             var stage = component.get('v.stage');
-
-           // var l1 = component.get('v.label1');
-           // var l2 = component.get('v.label2');
-           // //var l3 = component.get('v.label3');
-
-            //var c1 = component.get('v.color1');
-           // var c2 = component.get('v.color2');
-            //var c3 = component.get('v.color3');
             var project = component.get('v.project');
 
             console.log('Stage: ' + stage);
-            //console.log('Story: ' + story);
-            //console.log('Label 1: ' + l1);
-            //console.log('Label 2: ' + l2);
-            //console.log('Label 3: ' + l3);
-            //console.log('Color 1: ' + c1);
-           // console.log('Color 2: ' + c2);
-            //console.log('Color 3: ' + c3);
-
-
-
-            //var savingBacklogAction = component.get("c.addNewBacklog");
+          
             var savingColumnAction = component.get("c.addNewColumn");
 
             savingColumnAction.setParams({
@@ -216,66 +207,36 @@
                 }
 
             });
-            //console.log(savingBacklogAction);
-/*
-            savingBacklogAction.setParams({
-                "stage" : stage,
-                "story" : story,
-                "l1" : l1,
-                "l2": l2,
-                "l3": l3,
-                "c1": c1,
-                "c2": c2,
-                "c3": c3,
-                "project": project
-            });
-
-
-
-            savingBacklogAction.setCallback(this, function (response) {
-
-                var state = response.getState();
-
-                if (state === "SUCCESS"){
-
-                    var toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        "title": "Success",
-                        "message": "New card added to kanban board."
-                    });
-                    toastEvent.fire();
-                    
-                    helper.closeModel(component);
-                    $A.get("e.force:refreshView").fire();
-                }
-
-                else 
-                {
-                    var toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        "title": "Failed",
-                        "message": "New card could not be added to kanban board"
-                    });
-                    toastEvent.fire();
-                }
-
-            });
-
-            $A.enqueueAction(savingBacklogAction);
-            */
-
 
 
            $A.enqueueAction(savingColumnAction);  
-            
-            
-       // }
-        
-
-
-        
     },
 
+    deleteColumn: function (component, event, helper) {
+        
+        //var idx = component.get('v.Id');
+        var project = component.get('v.project');
+        var label = component.get('v.selectedValue');
+        var action = component.get("c.removeColumn");
 
+        //alert(idx);
+        action.setParams({ 
+                            'label' : label,
+                            'project': project 
+                        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                //component.set("v.backlogs", response.getReturnValue());
+                helper.closeModel(component);
+                $A.get("e.force:refreshView").fire();
+            } else {
+                console.log("Failed with state: " + state);
+            }
+        });
+    
+        $A.enqueueAction(action);
+    },
+    
     
 })
--- force-app/main/default/aura/KanbanProject/KanbanProjectHelper.js
+++ force-app/main/default/aura/KanbanProject/KanbanProjectHelper.js
@@ -1,7 +1,7 @@
 ({
     openModel: function (component) {
             // for Display Model,set the "isOpen" attribute to "true"
-            component.set("v.isOpen", true);
+            component.set("v.isOpenAddColumn", true);
         },
 
     closeModel: function (component) {
@@ -17,6 +17,28 @@
         component.set("v.color2", "");
         component.set("v.color3", "");
         
-        component.set("v.isOpen", false);
+        component.set("v.isOpenAddColumn", false);
+    },
+
+    //for delete column functionality
+    openModelDelCol: function (component) {
+        // for Display Model,set the "isOpen" attribute to "true"
+        component.set("v.isOpenDeleteColumn", true);
+    },
+
+    closeModelDelCol: function (component) {
+        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"
+
+        // component.set("v.Stage", "");
+        
+        component.set("v.story", "");
+        component.set("v.label1", "");
+        component.set("v.label2", "");
+        component.set("v.label3", "");
+        component.set("v.color1", "");
+        component.set("v.color2", "");
+        component.set("v.color3", "");
+        
+        component.set("v.isOpenDeleteColumn", false);
     },
 })
