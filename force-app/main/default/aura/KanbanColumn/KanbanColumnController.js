290df88393b2845fdd4c7090b9bfe29b0c92de7d
Francesco Ward <francesco@Francesco.local>
2020-01-10 15:01:28 -0500

    add card modal

--- force-app/main/default/aura/KanbanColumn/KanbanColumnController.js
+++ force-app/main/default/aura/KanbanColumn/KanbanColumnController.js
@@ -13,5 +13,119 @@
         droppedOnColumnEvent.fire();
     },
 
-    
+    openModelAddCard: function (component, event, helper) {
+        // for Display Model,set the "isOpen" attribute to "true"
+
+        helper.openModelAddTheCard(component);
+
+        var lab2 = component.find("label2");
+        console.log(lab2);
+
+        $A.util.addClass(lab2, 'toggle');
+
+
+        // component.get("label3").className = "slds-hidden";
+        // console.log(component.find("label2"));
+
+    },
+
+    closeModelAddCard: function (component, event, helper) {
+        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
+        helper.closeModelAddTheCard(component);
+
+    },
+
+    saveCard: function (component, event, helper) {
+
+        var story = component.get('v.story');
+
+        if (story === undefined || story === "" ){
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "title": "Save Failed",
+                "message": "Please make sure you have filled story."
+            });
+            toastEvent.fire();
+        }
+
+        else {
+
+            var column = component.get('v.colId');
+
+            var l1 = component.get('v.label1');
+            var l2 = component.get('v.label2');
+            var l3 = component.get('v.label3');
+
+            var c1 = component.get('v.color1');
+            var c2 = component.get('v.color2');
+            var c3 = component.get('v.color3');
+            var project = component.get('v.projectId');
+
+            console.log('ColId: ' + column);
+            console.log('Story: ' + story);
+            console.log('Label 1: ' + l1);
+            console.log('Label 2: ' + l2);
+            console.log('Label 3: ' + l3);
+            console.log('Color 1: ' + c1);
+            console.log('Color 2: ' + c2);
+            console.log('Color 3: ' + c3);
+
+
+
+            var savingBacklogAction = component.get('c.addNewBacklog');
+
+            console.log(savingBacklogAction);
+
+            savingBacklogAction.setParams({
+                "columnId" : column,
+                "story" : story,
+                "l1" : l1,
+                "l2": l2,
+                "l3": l3,
+                "c1": c1,
+                "c2": c2,
+                "c3": c3,
+                "project": project
+            });
+
+
+
+            savingBacklogAction.setCallback(this, function (response) {
+
+                var state = response.getState();
+
+                if (state === "SUCCESS"){
+
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "title": "Success",
+                        "message": "New card added to kanban board."
+                    });
+                    toastEvent.fire();
+                    
+                    helper.closeModelAddTheCard(component);
+                    $A.get("e.force:refreshView").fire();
+                }
+
+                else 
+                {
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "title": "Failed",
+                        "message": "New card could not be added to kanban board"
+                    });
+                    toastEvent.fire();
+                }
+
+            });
+
+            $A.enqueueAction(savingBacklogAction);
+            
+            
+        }
+        
+
+
+        
+    },
 })
