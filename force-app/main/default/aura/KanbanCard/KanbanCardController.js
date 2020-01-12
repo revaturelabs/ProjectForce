9ce7aae0306d521d9bd6b1e966c848dfdc56df43
Francesco Ward <francesco@Francesco.local>
2020-01-12 15:58:16 -0500

    remove card functionality

--- force-app/main/default/aura/KanbanCard/KanbanCardController.js
+++ force-app/main/default/aura/KanbanCard/KanbanCardController.js
@@ -8,4 +8,50 @@
         var backlog = component.get('v.backlog');
       	event.dataTransfer.setData('draggedBacklog', JSON.stringify(backlog));
 	},
+
+	deleteCard : function(component, event, helper) {
+		var cardId = component.get('v.backlogId');
+		var story = component.get('v.Story');
+
+		var deleteCardAction = component.get('c.removeCard');
+
+           
+
+		deleteCardAction.setParams({
+                "card" : cardId,
+				"story" : story });
+				
+				deleteCardAction.setCallback(this, function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS"){
+
+                    //component.set("v.backlogs", response.getReturnValue());
+					// rerender the board or column
+					/*
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "title": "Success",
+                        "message": "New card added to kanban board."
+                    });
+					toastEvent.fire();
+					*/
+                    
+                    $A.get("e.force:refreshView").fire();
+                }
+                else 
+                {
+					/*
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "title": "Failed",
+                        "message": "New card could not be added to kanban board"
+                    });
+					toastEvent.fire();
+					*/
+                }
+
+            });
+
+            $A.enqueueAction(deleteCardAction);  
+	},
 })
