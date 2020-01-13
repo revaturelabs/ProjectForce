30bb2d66aa6a6030b3d81dcdc51015a4b2480ef7
meretribia <brady.achterberg@gmail.com>
2020-01-13 11:20:43 -0500

    Bring down recent changes from development branch and also update tests
    Merge in the stuff from the Testing branch that had just been merged from the Dev branch covering Bailey and Francesco's recent changes; also refactored Data Factory a bunch, improved its delete method and test class, and started rebuilding KanbanProjectTest.  Coverage back down to 60%.

--- force-app/main/default/aura/KanbanCard/KanbanCardController.js
+++ force-app/main/default/aura/KanbanCard/KanbanCardController.js
@@ -1,11 +1,53 @@
 ({
 	doInit: function(component, event, helper){
 
-		},
+	},
 	
 	onDragStart : function(component, event, helper) {
 		event.dataTransfer.dropEffect = "move";
         var backlog = component.get('v.backlog');
       	event.dataTransfer.setData('draggedBacklog', JSON.stringify(backlog));
+	},
+
+	deleteCard : function(component, event, helper) {
+		var cardId = component.get('v.backlogId');
+		var story = component.get('v.Story');
+
+		var deleteCardAction = component.get('c.removeCard');
+
+		deleteCardAction.setParams({
+                "card" : cardId,
+				"story" : story });
+				
+				deleteCardAction.setCallback(this, function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS"){
+
+                    $A.get("e.force:refreshView").fire();
+                }
+            });
+
+		$A.enqueueAction(deleteCardAction);  
+	},
+
+	updateStory : function(component, event, helper) {
+		var cardId = component.get('v.backlogId');
+		var story = component.get('v.Story');
+
+		var updateCardAction = component.get('c.updateCard');
+
+		updateCardAction.setParams({
+                "card" : cardId,
+				"story" : story });
+				
+				updateCardAction.setCallback(this, function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS"){
+
+                    $A.get("e.force:refreshView").fire();
+                }
+            });
+
+		$A.enqueueAction(updateCardAction);  
 	},
 })
